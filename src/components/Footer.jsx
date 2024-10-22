b5276dd2b6fa46bc3018e34bf81af6e7bf1d988d
Bernardo <bernardomaniglia04@gmail.com>
2024-10-22 07:55:14 -0300

    Criando NavBar e Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Escreve sobre oque você tem interrese!</h3>
+      <p>Mini Blog &copy; 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
